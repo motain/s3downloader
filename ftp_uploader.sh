a2ee5d19dae9d8c0ea40555d62d831d1a0251b4a
Eldar Rakhimberdin <eldar.rakhimberdin@onefootball.com>
2017-01-20 14:09:38 +0100

    Added a bash script which downloads file from s3 and upload them to frp_receiver server

---
+++ ftp_uploader.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+USAGE='
+	This script downloads file from amazon s3 and upload them to ftp_receiver server.
+
+	USAGE:
+		./ftp_uploader.sh "FTP_RECEIVER_IP" "prefix" "competition name"
+
+	EXAMPLE:
+		./ftp_uploader.sh "127.0.0.1" "backup/standings/scoreradar/stats/" "Mexico.CopaMX"
+
+'
+
+USER=erakhimberdin
+WATCHING_FOLDER=/sharedstorage/middleware/pub/push/scoreradar/stats
+ILIGAHOME=/home/iliga
+DOWNLOAD_DIR=./tmp/s3feeds
+
+COMMAND="sudo mkdir -p $ILIGAHOME/tmp_feeds && \
+		sudo mv /home/$USER/tmp_feeds/*.xml $ILIGAHOME/tmp_feeds && \
+		sudo chown iliga:iliga $ILIGAHOME/tmp_feeds/*.xml && \
+		sudo mv $ILIGAHOME/tmp_feeds/*.xml $WATCHING_FOLDER && \
+		sleep 1 && \
+		echo 'Number of files in watching folder after 1 second:' && \
+		sudo ls -lA $WATCHING_FOLDER | wc -l && \
+		sleep 5 && \
+		echo 'Number of files in the watching folder after 5 seconds:' && \
+		sudo ls -lA $WATCHING_FOLDER | wc -l
+		"
+
+if [[ -z "$1" ]] || [[ -z "$2" ]] || [[ -z "$3" ]]; then
+  printf "$USAGE"
+else
+	FTP_RECEIVER_IP=$1
+	echo "1) Downloading files from s3" && \
+	rm -f $DOWNLOAD_DIR/*.xml && \
+	go run main.go -bucket=motain-feeds -dir=$DOWNLOAD_DIR -prefix=$2 -regexp=$3 && \
+	read -p "Do you want to upload these files to the ftp_receiver server? (y/n): " -r
+	if [[ "$REPLY" = "y" ]]
+	then
+		echo "2) Uploading files ssh $USER@$FTP_RECEIVER_IP" && \
+		rsync -v -r $DOWNLOAD_DIR/*.xml $USER@$FTP_RECEIVER_IP:/home/$USER/tmp_feeds && \
+		echo "3) Moving files to $WATCHING_FOLDER"  && \
+		ssh $USER@$FTP_RECEIVER_IP $COMMAND
+	fi
+	echo "4) Finish"
+
+fi
